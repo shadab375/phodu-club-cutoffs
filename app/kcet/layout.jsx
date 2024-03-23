35b6bf9820e62184b71445b492e66cada67625f5
Shadboy9625 <shadabkalim375@gmail.com>
2024-03-23 16:28:28 +0530

    'done'

---
+++ app/kcet/layout.jsx
@@ -0,0 +1,31 @@
+import { Inter } from "next/font/google";
+const inter = Inter({ subsets: ["latin"] });
+export const metadata = {
+  title: "KCET Rank vs Branch vs Cutoff Predictor (2024)",
+  description:
+    "Our KCET Rank vs Branch vs Cutoff Predictor Tool can help you estimate your potential rank & explore branch options based on previous year's cut-offs.",
+};
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" style={{ height: "100%" }}>
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+      </head>
+      <body
+        className={inter.className}
+        style={{ height: "100%", margin: "0", padding: "0" }}
+      >
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            minHeight: "100%",
+          }}
+        >
+          <main style={{ flex: "1 0 auto" }}>{children}</main>
+        </div>
+      </body>
+    </html>
+  );
+}
