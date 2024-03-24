7a1d33a5fb23816d0fba50061091e0df68987ab2
Shadboy9625 <shadabkalim375@gmail.com>
2024-03-25 00:43:23 +0530

    'done'

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,6 +13,7 @@ export default function RootLayout({ children }) {
       <head>
         <title>{metadata.title}</title>
         <meta name="description" content={metadata.description} />
+        <link rel="icon" href="/favicon.ico" />
       </head>
       <body
         className={inter.className}
